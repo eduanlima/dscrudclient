4b5249c945031315ab88ed17f096ebc8c5ff6025
eduanlima <eduandev@gmail.com>
2022-02-05 20:43:39 -0300

    Created endpoint to update client.

--- backend/src/main/java/com/eduanlima/dscrudclient/resources/ClientResource.java
+++ backend/src/main/java/com/eduanlima/dscrudclient/resources/ClientResource.java
@@ -9,7 +9,9 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +45,12 @@ public class ClientResource {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/eduanlima/dscrudclient/services/ClientService.java
+++ backend/src/main/java/com/eduanlima/dscrudclient/services/ClientService.java
@@ -30,6 +30,20 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			
+			return new ClientDTO(entity);
+			
+		}catch(Exception error) {
+			throw new RuntimeException(error);
+		}
+	}
+	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
